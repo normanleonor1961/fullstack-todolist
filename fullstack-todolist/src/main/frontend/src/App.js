123b4d983647f0b30d08805f12232b5c3775c264
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-09-29 17:31:26 +0800

    Add react frontend

---
+++ fullstack-todolist/src/main/frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoList from './components/TodoList';
+import Auth from './components/authentication/Auth';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const activeUser = useSelector((state) => state.activeUser)
+  return (
+    <div className="App">
+      {activeUser.email ? <TodoList /> : <Auth />}
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstack-todolist/src/main/frontend/src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { bindActionCreators } from "redux";
+import TodoBody from "./TodoBody";
+import TodoHeader from "./TodoHeader";
+import * as actionLogin from "../redux/actions/actionLogin";
+import { useDispatch } from "react-redux";
+
+export default function TodoList() {
+  const { logoutUser } = bindActionCreators(actionLogin, useDispatch());
+  return (
+    <div className="todo-body">
+      <h1>Task List 2022</h1>
+      <TodoHeader />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <TodoBody />
+      <br />
+      <br />
+      <Button onClick={() => logoutUser()}>LOGOUT</Button>
+    </div>
+  );
+}
